3d998847796952a9877ce6bb86c17859922e570d
Lir00f <nikolaenko.e.i.21@gmail.com>
2022-11-16 20:50:27 +0300

    4.4

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
