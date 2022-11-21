82c6e8be68b434d1f088468051242324c40a035c
Lir00f <nikolaenko.e.i.21@gmail.com>
2022-11-21 21:48:38 +0300

    mongodb improvments

---
+++ createDB.js
@@ -0,0 +1,21 @@
+var MongoClient = require('mongodb').MongoClient
+var data = require("./data.js").data
+
+const uri = "mongodb://127.0.0.1:27017/"
+const client = new MongoClient(uri)
+async function run() {
+    try  {
+        await client.connect();
+        var database = client.db("midnightclub");
+        database.dropDatabase()
+        database = client.db("midnightclub");
+        const cars = database.collection("cars");
+        const result = await cars.insertOne({name: "Supra"});
+        for (const key in result) {
+            console.log(`${key}: ${result[key]}`);
+            }
+    } finally {
+        await client.close();
+    }
+}
+run()
---
+++ data.js
@@ -0,0 +1,23 @@
+var data = [{
+    title: 'Toyota Supra A70',
+    nick: 'supra',
+    avatar: '/public/images/supraa70.jpg',
+    desc: 'In February 1986, the bonds between the Celica and the Supra were cut; they were now two completely different models. The Celica changed to a front-wheel drive layout, using the Toyota T platform associated with the Corona, while the Supra kept its rear-wheel-drive layout. The engine was updated to a more powerful 2,954 cc (3.0 L) inline-six engine rated at 149 kW (200 hp; 203 PS). Although initially only available with naturally aspirated engines, a turbocharged version was added in the 1987 model year. The Supra was now mechanically related to the Soarer.'
+    },
+    {
+    title: 'Nissan S130z',
+    nick: 'nissans130z',
+    avatar: '/public/images/nissans130z.jpg',
+    desc: 'The Nissan S130 is a sports coupé produced by Nissan in Japan from 1978 until 1983. It was sold as the Datsun 280ZX, Nissan Fairlady Z and Nissan Fairlady 280Z, depending on the market. In Japan, it was exclusive to Nissan Bluebird Store locations. It was the second generation Z-car, replacing the Nissan S30 in late 1978. The 280ZX was the first time the by Nissan subscript was badged alongside the Datsun logo, along with Nissan trucks. The 280ZX was Motor Trends import car of the year for 1979. The 280ZX was replaced by the Nissan 300ZX in 1984.'
+    },
+    {
+    title: 'Porsche 930 Turbo',
+    nick: 'porsche930turbo',
+    avatar: '/public/images/porsche930turbo.jpg',
+    desc: 'The Porsche 930 is a sports car manufactured by German automobile manufacturer Porsche between 1975 and 1989, known to the public as the 911 Turbo. It was the makers top-of-the-range 911 model for its entire production duration and, at the time of its introduction, was the fastest production car available in Germany.'
+    }
+    ];
+    
+    
+    module.exports.data = data;
+    
