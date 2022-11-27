32ce3993057f65f2288e8662fbfded7c716ec2f5
Lir00f <nikolaenko.e.i.21@gmail.com>
2022-11-27 21:28:00 +0300

    8.3

--- routes/cars.js
+++ routes/cars.js
@@ -6,9 +6,18 @@ router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с cars');
 });
 
-module.exports = router;
-
 /* Страница машин */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-});
+router.get('/:nick', function(req, res, next) {
+    Car.findOne({nick:req.params.nick}, function(err,car){
+        if(err) return next(err)
+        if(!car) return next(new Error("There was no such cаr in MidNightClub, maybe you made a mistake in your request?"))
+        res.render('car', {
+            title: car.title,
+            picture: car.avatar,
+            desc: car.desc
+        })
+    })
+})
+
+
+module.exports = router;
