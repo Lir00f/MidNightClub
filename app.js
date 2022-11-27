df367b73d646397a05c4ab740fffbaada48e2c18
Lir00f <nikolaenko.e.i.21@gmail.com>
2022-11-27 20:58:22 +0300

    8.1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/midnightclub')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
