2d46941ae5b4cc12b1ee18f021d9e4cddfd8c1f9
Lir00f <nikolaenko.e.i.21@gmail.com>
2022-11-30 00:04:28 +0300

    8.4(+fixes)

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/test1')
-var Car = require("./models/car").Car
+mongoose.connect('mongodb://127.0.0.1/test1')
+var Car = require("../models/car").Car
 
 
 var car = new Car({
--- routes/cars.js
+++ routes/cars.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Car = require("../models/car").Car
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
