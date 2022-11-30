9da17d0dc41e2107e6302bba662595ea276501f9
Lir00f <nikolaenko.e.i.21@gmail.com>
2022-11-30 17:34:02 +0300

    доделал 8Ñ.5

--- routes/cars.js
+++ routes/cars.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Car = require("../models/car").Car
+var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,17 +9,30 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница машин */
+
+
 router.get('/:nick', function(req, res, next) {
-    Car.findOne({nick:req.params.nick}, function(err,car){
-        if(err) return next(err)
-        if(!car) return next(new Error("There was no such cаr in MidNightClub, maybe you made a mistake in your request?"))
-        res.render('car', {
-            title: car.title,
-            picture: car.avatar,
-            desc: car.desc
+    async.parallel([
+            function(callback){
+                Car.findOne({nick:req.params.nick}, callback)
+            },
+            function(callback){
+                Car.find({},{_id:0,title:1,nick:1},callback)
+            }
+        ],
+        function(err,result){
+            if(err) return next(err)
+            var car = result[0]
+            var cars = result[1] || []
+            if(!car) return next(new Error("There was no such cаr in MidNightClub, maybe you made a mistake in your request?"))
+            res.render('car', {
+                title: car.title,
+                picture: car.avatar,
+                desc: car.desc,
+                menu: cars
+            });
         })
-    })
-})
-
-
-module.exports = router;
+  })
+  
+  module.exports = router
+  
--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Car = require("../models/car").Car
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    Car.find({},{_id:0,title:1,nick:1},function(err,menu){
+        res.render('index', {
+                                title: 'Express',
+                                menu: menu
+                            });
+    })
+
 });
-/* Страница Supra a70 
-router.get('/supraa70', function(req, res, next) {
-  res.render('car', {
-    title: "Toyota Supra A70",
-    picture: "images/supraa70.jpg",
-    desc: "In February 1986, the bonds between the Celica and the Supra were cut; they were now two completely different models. The Celica changed to a front-wheel drive layout, using the Toyota T platform associated with the Corona, while the Supra kept its rear-wheel-drive layout. The engine was updated to a more powerful 2,954 cc (3.0 L) inline-six engine rated at 149 kW (200 hp; 203 PS). Although initially only available with naturally aspirated engines, a turbocharged version was added in the 1987 model year. The Supra was now mechanically related to the Soarer."
-  })
-  res.send("<h1>About Supra A70</h1>")
-});
-/* Страница Nissan s130z 
-router.get('/nissans130z', function(req, res, next) {
-  res.render('car', {
-    title: "Nissan S130z",
-    picture: "images/nissans130z.jpg",
-    desc: "The Nissan S130 is a sports coupé produced by Nissan in Japan from 1978 until 1983. It was sold as the Datsun 280ZX, Nissan Fairlady Z and Nissan Fairlady 280Z, depending on the market. In Japan, it was exclusive to Nissan Bluebird Store locations. It was the second generation Z-car, replacing the Nissan S30 in late 1978. The 280ZX was the first time the by Nissan subscript was badged alongside the Datsun logo, along with Nissan trucks. The 280ZX was Motor Trend's import car of the year for 1979. The 280ZX was replaced by the Nissan 300ZX in 1984."
-  })
-  res.send("<h1>About Nissan S130z</h1>")
-});
-/* Страница Porsche 930 Turbo 
-router.get('/porsche930turbo', function(req, res, next) {
-  res.render('car', {
-    title: "Porsche 930 Turbo",
-    picture: "images/porsche930turbo.jpg",
-    desc: "The Porsche 930 is a sports car manufactured by German automobile manufacturer Porsche between 1975 and 1989, known to the public as the 911 Turbo. It was the maker's top-of-the-range 911 model for its entire production duration and, at the time of its introduction, was the fastest production car available in Germany."
-  })
-  res.send("<h1>About Porsche 930 Turbo</h1>")
-});
-*/
+
 module.exports = router;
