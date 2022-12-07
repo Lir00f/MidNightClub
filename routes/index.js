333486d329216a195b289ebd01b3a03fcc17936f
Lir00f <nikolaenko.e.i.21@gmail.com>
2022-12-07 20:30:31 +0300

    9.2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router()
 var Car = require("../models/car").Car
 
 
-/* GET home page. */
+
+
 router.get('/', function(req, res, next) {
     Car.find({},{_id:0,title:1,nick:1},function(err,menu){
         res.render('index', {
@@ -13,5 +14,11 @@ router.get('/', function(req, res, next) {
     })
 
 });
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
+});
+
 
 module.exports = router;
