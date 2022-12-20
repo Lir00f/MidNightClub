9b15e85d79764a944c3e7cd7b27c6e02f10f82c4
Lir00f <nikolaenko.e.i.21@gmail.com>
2022-12-20 22:05:47 +0300

    10.4

--- models/user.js
+++ models/user.js
@@ -33,4 +33,8 @@ schemaUser.methods.encryptPassword = function(password){
     return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
 }
 
+schemaUser.methods.checkPassword = function(password){
+    return this.encryptPassword(password) === this.hashedPassword
+}
+
 module.exports.User = mongoose.model("User",schemaUser);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var Car = require("../models/car").Car
-
+var User = require("./../models/user").User
 
 
 
@@ -27,9 +27,24 @@ router.get('/logreg', function(req, res, next) {
 router.post('/logreg', function(req, res, next) {
     var username = req.body.username
     var password = req.body.password
-
-
+  User.findOne({username:username},function(err,user){
+      if(err) return next(err)
+      if(user){
+          if(user.checkPassword(password)){
+              req.session.user = user._id
+              res.redirect('/')
+          } else {
+                    res.render('logreg', {title: 'Вход'})
+          }
+      } else {
+          var user = new User({username:username,password:password})
+          user.save(function(err,user){
+              if(err) return next(err)
+              req.session.user = user._id
+              res.redirect('/')
+          })        
+    }
+  })
 });
-
 
 module.exports = router;
