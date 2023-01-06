3679270bfffb93887e02aad936144e9795f9f0b8
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-01-06 21:18:53 +0300

    data to menu

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,11 +1,14 @@
 var Car = require("./../models/car").Car
 
-module.exports = function(req,res,next){
-    res.locals.nav = []
 
-    Car.find({},{_id:0,title:1,nick:1},function(err,result){
-        if(err) throw err
-        res.locals.nav = result
-        next()
-    })
+
+module.exports = function(req,res,next){
+res.locals.nav = []
+
+
+Car.find(null,{_id:0,title:1,nick:1},function(err,result){
+if(err) throw err
+res.locals.nav = result
+next()
+})
 }
